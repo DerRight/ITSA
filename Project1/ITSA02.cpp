4580ece70a66febf06b9e4765d157ad24f408d08
DerRight <zxc123456789123456@gmail.com>
2023-03-02 17:15:40 +0800

    master

--- Project1/ITSA02.cpp
+++ Project1/ITSA02.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 
 int main() {
-	int mile;
-	double km;
-
-	cin >> mile;
-	km = 1.6 * mile;
-	cout << fixed << setprecision(1) << km << endl;
 	return 0;
 }
