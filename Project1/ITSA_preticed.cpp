b82c1e87b2e996d6991220be9b46b0080fe6cb6a
阿虎 <zxcr552@gmail.com>
2023-03-06 13:05:22 +0800

    itsa03_A

--- Project1/ITSA_preticed.cpp
+++ Project1/ITSA_preticed.cpp
@@ -5,7 +5,10 @@ int main() {
 	int X, Y;
 	int r = 100;
 	cin >> X >> Y;
-	// X*X + Y*Y = R*R
-	
+	ori = X * X + Y * Y;
+	val = R * R;
+	if ori > val
+		cout << "outside";
+	else cout << "inside";
 	return 0;
 }
