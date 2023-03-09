7ae04b1448c806a191ace5f5ff3e114121764af7
DerRight <zxc123456789123456@gmail.com>
2023-03-09 16:57:42 +0800

    ITSA04

--- Project1/ITSA_preticed.cpp
+++ Project1/ITSA_preticed.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void ParkingCost(int START_HR, int START_MIN, int END_HR, int END_MIN) {
+	int TOTAL_HR = 0, TOTAL_MIN = 0;
+	TOTAL_HR = START_HR - END_HR;
+	TOTAL_MIN = START_MIN - END_MIN;
+	cout << TOTAL_HR << TOTAL_MIN << endl;
+}
 
+int main() {
+	int START_HR, START_MIN;
+	int END_HR, END_MIN;
+
+	cin >> START_HR >> START_MIN;
+	cin >> END_HR >> END_MIN;
+	ParkingCost(START_HR, START_MIN, END_HR, END_MIN);
 	return 0;
 }
