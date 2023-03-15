0116458e84489c75d11973401e8b38b9633e657b
DerRight <zxc123456789123456@gmail.com>
2023-03-15 17:43:49 +0800

    ITSA04_AAAA

--- Project1/ITSA_preticed.cpp
+++ Project1/ITSA_preticed.cpp
@@ -6,10 +6,10 @@ void ParkingCost(int START_HR, int START_MIN, int END_HR, int END_MIN) {
 	int TOTAL_ParkingTime = 0, TOTAL_ParkingTime_Half = 0;
 	TOTAL_ParkingTime = ((END_HR * 60) + END_MIN) - ((START_HR * 60) + START_MIN); // min.
 
-	TOTAL_ParkingTime_Half = TOTAL_ParkingTime / 30;
-	if (TOTAL_ParkingTime < 120) Cost = 30 * TOTAL_ParkingTime_Half; // $120
-	else if ((120 <= TOTAL_ParkingTime) && (TOTAL_ParkingTime < 240)) Cost = 120 + (40 * (TOTAL_ParkingTime_Half - 120));
-	else if (TOTAL_ParkingTime >= 240) Cost = 120 + 160 + (60 * (TOTAL_ParkingTime_Half - 240));
+	//TOTAL_ParkingTime_Half = TOTAL_ParkingTime / 30;
+	if (TOTAL_ParkingTime < 120) Cost = 30 * (TOTAL_ParkingTime / 30); // $120
+	else if ((120 <= TOTAL_ParkingTime) && (TOTAL_ParkingTime < 240)) Cost = 120 + (((TOTAL_ParkingTime - 120) / 30) * 40);
+	else if (TOTAL_ParkingTime >= 240) Cost = 120 + 160 + (((TOTAL_ParkingTime - 240) / 30) * 60);
 	else if (TOTAL_ParkingTime < 30) Cost = 0;
 	cout << Cost << endl;
 }
