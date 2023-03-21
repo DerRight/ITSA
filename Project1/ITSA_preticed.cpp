3f526bc9536aab5f1b05de068d555b383a3ac6f2
DerRight <zxc123456789123456@gmail.com>
2023-03-22 00:45:51 +0800

    itsa08_A

--- Project1/ITSA_preticed.cpp
+++ Project1/ITSA_preticed.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
+	int num, count = 0;
 
+	cin >> num;
+	for (int i = 2; i < num; i++)
+		if (num % i == 0) count = count + 1;
+	if (count == 0)
+		cout << "YES" << endl;
+	else cout << "NO" << endl;
 	return 0;
 }
