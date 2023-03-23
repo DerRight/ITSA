65044b270fa18d1b9846e5594d6195e0fe3fbe23
DerRight <zxc123456789123456@gmail.com>
2023-03-23 16:39:44 +0800

    itsa05

--- Project1/ITSA_preticed.cpp
+++ Project1/ITSA_preticed.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <bitset>
 using namespace std;
 
-int main() {
+void DecTObin(int Dec) {
+	int Tran_Bin;
+	if (Dec < 0) Dec += 256;
+	bitset<10> bit(Dec);
+	cout << Dec << endl;
+}
 
+int main() {
+	int Dec_num;
+
+	cin >> Dec_num;
+	DecTObin(Dec_num);
 	return 0;
 }
