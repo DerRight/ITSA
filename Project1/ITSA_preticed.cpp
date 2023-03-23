124153e9f913916eb29a8841992877fedb3f5103
DerRight <zxc123456789123456@gmail.com>
2023-03-23 15:30:28 +0800

    ITSA03_A

--- Project1/ITSA_preticed.cpp
+++ Project1/ITSA_preticed.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main() {
 	int X, Y;
+	int ori, val;
 	int r = 100;
 	cin >> X >> Y;
 	ori = X * X + Y * Y;
-	val = R * R;
-	if ori > val
-		cout << "outside";
+	val = r * r;
+	if (ori > val) cout << "outside";
 	else cout << "inside";
 	return 0;
 }
