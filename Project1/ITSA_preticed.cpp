4bbe63e6475d5fc1640ee7eab8210246fa290288
DerRight <zxc123456789123456@gmail.com>
2023-03-23 15:32:22 +0800

    ITSA03_AA

--- Project1/ITSA_preticed.cpp
+++ Project1/ITSA_preticed.cpp
@@ -8,7 +8,7 @@ int main() {
 	cin >> X >> Y;
 	ori = X * X + Y * Y;
 	val = r * r;
-	if (ori > val) cout << "outside";
-	else cout << "inside";
+	if (ori > val) cout << "outside" << endl;
+	else cout << "inside" << endl;
 	return 0;
 }
